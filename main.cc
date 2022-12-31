6bdb1aae2e28a5b79224ff191387f7bbda14e0c3
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2022-12-31 14:49:31 -0600

    finish main

--- main.cc
+++ main.cc
@@ -1,15 +1,39 @@
 #include "unpack.cc"
 #include "reader.cc"
 #include "writer.cc"
+#include "filesystem.hpp"
 
-int main() {
-    Writer writer{"test.csv"};
+void HandleBCSV(const ghc::filesystem::path& arg, const ghc::filesystem::path& dir) {
+    Reader reader{arg.string().c_str(), (dir / "lookup_supermariogalaxy.txt").string().c_str()};
+    ghc::filesystem::path out{arg};
+    reader.WriteCSV(out.replace_extension(".csv").string().c_str());
+}
+
+void HandleCSV(const ghc::filesystem::path& arg) {
+    Writer writer{arg.string().c_str()};
     writer.GenerateFields();
     auto vals = writer.GetValues();
     auto table = writer.CreateStringTable(vals);
     auto buffer = writer.CreateBuffer(table);
     auto doff = writer.FillHeaderAndFields(buffer);
     writer.FillFieldTable(buffer, doff, vals);
-    writer.WriteBCSV("test.bcsv", buffer);
+    ghc::filesystem::path out{arg};
+    writer.WriteBCSV(out.replace_extension(".bcsv").string().c_str(), buffer);
+}
+
+int main(int argc, char* argv[]) {
+    const ghc::filesystem::path exe = argv[0];
+    const auto dir = exe.parent_path();
+    std::vector<std::string> args{(size_t)argc - 1};
+    std::copy(argv + 1, argv + 1 + argc - 1, args.begin());
+    for (auto arg : args) {
+        ghc::filesystem::path p{arg};
+        auto ext = p.extension().string();
+        if (ext == ".bcsv" || ext == ".tbl" || ext == ".banmt" || ext == "") {
+            HandleBCSV(arg, dir);
+        } else if (ext == ".csv") {
+            HandleCSV(arg);
+        }
+    }
     return 0;
 }
