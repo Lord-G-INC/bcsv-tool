57558a67ce4890c1936abc3f36392d7286b66afa
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2025-02-28 23:14:16 -0600

    bump libbcsv, command line args updated to be far cleaner.

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,79 @@
 use clap::*;
 use libbcsv::*;
+use libbcsv::Endian as BinRWEndian;
 use std::{io::Cursor, path::*};
+
+#[derive(Debug, Clone, Copy, ValueEnum)]
+enum Endian {
+    Big,
+    Little
+}
+
+impl Endian {
+    #[cfg(target_endian = "big")]
+    const NATIVE : Endian = Endian::Big;
+    #[cfg(target_endian = "little")]
+    const NATIVE : Endian = Endian::Little;
+}
+
+impl From<Endian> for BinRWEndian {
+    fn from(value: Endian) -> Self {
+        match value {
+            Endian::Big => Self::Big,
+            Endian::Little => Self::Little
+        }
+    }
+}
+
+impl std::ops::Not for Endian {
+    type Output = Self;
+    fn not(self) -> Self::Output {
+        match self {
+            Self::Big => Self::Little,
+            Self::Little => Self::Big
+        }
+    }
+}
 
 #[derive(Debug, Default, Clone, Parser)]
 #[command(version)]
 #[command(about="Converts bcsv files to and from csv.")]
 struct ProgArgs {
-    #[arg(short, long)]
     /// The file to convert from. (Supported: *.csv, any bcsv extension)
-    pub infile: String,
-    #[arg(short, long)]
+    pub infile: PathBuf,
     /// The file to convert to. (Supported: *.csv, *.xlsx, any bcsv extension)
-    pub outfile: String,
+    pub outfile: PathBuf,
     #[arg(short, long)]
     /// The hash lookup file to use, 
     /// 
     /// this SHOULD be provided when doing bcsv to csv,
     /// but isn't required.
-    pub lookup: Option<String>,
+    pub lookup: Option<PathBuf>,
     #[arg(short, long)]
-    /// If enabled, use the OPPOSITE endian to the system's endian.
-    /// 
-    /// Little Endian becomes Big Endian, and vice versa.
-    pub endian: bool,
+    /// The Endian to use for parsing. Defaults to the reverse Native endian.
+    pub endian: Option<Endian>,
     #[arg(short, long)]
     /// If enabled, the SHORT and CHAR datatypes will be UNSIGNED.
     pub unsigned: bool,
-    #[arg(short, long)]
-    /// Optional delimiter for CSV reading/writing
-    pub delim: Option<char>
+    #[arg(short, long, default_value_t = ',')]
+    /// Delimiter for CSV reading/writing
+    pub delim: char
 }
 
 
 fn main() -> Result<(), BcsvError> {
     let args = ProgArgs::parse();
-    let inpath = Path::new(&args.infile);
-    let outpath = Path::new(&args.outfile);
+    let inpath = &args.infile;
+    let outpath = &args.outfile;
     let lookup = &args.lookup;
     let signed = !args.unsigned;
-    let delim = match args.delim {
-        Some(d) => d,
-        None => ','
-    };
-    let endian = match args.endian {
-        false => Endian::NATIVE,
-        true => match Endian::NATIVE {
-            Endian::Big => Endian::Little,
-            Endian::Little => Endian::Big
-        }
-    };
+    let delim = args.delim;
+    let endian = args.endian.unwrap_or(!Endian::NATIVE);
     if let Some(inext) = inpath.extension() {
         if inext.to_string_lossy() == "csv" {
             // csv to bcsv, extension for output not checked because bcsv has a few oddball extensions
             let csv = csv_parse::CSV::from_path(inpath, delim)?;
-            let data = csv.create_bcsv().to_bytes(endian)?;
+            let data = csv.create_bcsv().to_bytes(endian.into())?;
             std::fs::write(outpath, data)?;
         }
         if let Some(oext) = outpath.extension() {
@@ -65,7 +85,7 @@ fn main() -> Result<(), BcsvError> {
                 let data = std::fs::read(inpath)?;
                 let mut reader = Cursor::new(data);
                 let mut bcsv = types::BCSV::new();
-                bcsv.read(&mut reader, endian)?;
+                bcsv.read(&mut reader, endian.into())?;
                 let hashes = lookup.as_ref()
                 .map(|x| hash::read_hashes(x).unwrap_or_default()).unwrap_or_default();
                 let text = bcsv.convert_to_csv(&hashes, signed, delim);
@@ -76,7 +96,7 @@ fn main() -> Result<(), BcsvError> {
                 let data = std::fs::read(inpath)?;
                 let mut reader = Cursor::new(data);
                 let mut bcsv = types::BCSV::new();
-                bcsv.read(&mut reader, endian)?;
+                bcsv.read(&mut reader, endian.into())?;
                 let hashes = lookup.as_ref()
                 .map(|x| hash::read_hashes(x).unwrap_or_default()).unwrap_or_default();
                 bcsv.convert_to_xlsx(outpath.as_os_str().to_string_lossy(), &hashes, signed)?;
@@ -91,7 +111,7 @@ fn main() -> Result<(), BcsvError> {
             let data = std::fs::read(inpath)?;
             let mut reader = Cursor::new(data);
             let mut bcsv = types::BCSV::new();
-            bcsv.read(&mut reader, endian)?;
+            bcsv.read(&mut reader, endian.into())?;
             let hashes = lookup.as_ref()
             .map(|x| hash::read_hashes(x).unwrap_or_default()).unwrap_or_default();
             let text = bcsv.convert_to_csv(&hashes, signed, delim);
@@ -101,7 +121,7 @@ fn main() -> Result<(), BcsvError> {
             let data = std::fs::read(inpath)?;
             let mut reader = Cursor::new(data);
             let mut bcsv = types::BCSV::new();
-            bcsv.read(&mut reader, endian)?;
+            bcsv.read(&mut reader, endian.into())?;
             let hashes = lookup.as_ref()
             .map(|x| hash::read_hashes(x).unwrap_or_default()).unwrap_or_default();
             bcsv.convert_to_xlsx(outpath.as_os_str().to_string_lossy(), &hashes, signed)?;
